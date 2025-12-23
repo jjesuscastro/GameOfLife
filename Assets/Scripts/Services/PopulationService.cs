7fd8f50407d9792dc9490007d40ba058f64a48af
Jesus Castro <jjoesei.castro@gmail.com>
2025-12-24 00:01:46 +0800

    Update to Burst Compiler and Jobs

--- Assets/Scripts/Services/PopulationService.cs
+++ Assets/Scripts/Services/PopulationService.cs
@@ -1,6 +1,9 @@
 using Models.Interfaces;
 using ScriptableObjects;
 using Services.Interfaces;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
 
 namespace Services {
     public class PopulationService : IPopulationService {
@@ -14,103 +17,65 @@ namespace Services {
         }
         
         /// <summary>
-        /// Get the next generation of the grid
+        /// Counts the neighbors and updates the next generation of the grid
+        /// using Burst Compiler
         /// </summary>
-        /// <param name="grid"></param>
+        /// <param name="grid">
+        /// Reference to the current grid
+        /// </param>
         public void NextGeneration(IGrid grid) {
-            int requiredSize = this.variables.width * this.variables.height;
-            if (this.neighborsGrid == null || this.neighborsGrid.Length != requiredSize) {
-                this.neighborsGrid = new int[requiredSize];
-            }
-            
-            CountNeighbors(grid);
-            ControlPopulation(grid);
-        }
+            int length = grid.Cells.Length;
+            NativeArray<int> currentCells = new NativeArray<int>(grid.Cells, Allocator.TempJob);
+            NativeArray<int> nextCells = new NativeArray<int>(length, Allocator.TempJob);
 
-        /// <summary>
-        /// Count the neighbors of each cell in the grid
-        /// </summary>
-        /// <param name="grid"></param>
-        private void CountNeighbors(IGrid grid) {
-            int[] cells = grid.Cells;
-            int width = this.variables.width;
-            int height = this.variables.height;
+            SimulationJob job = new SimulationJob {
+                currentCells = currentCells,
+                nextCells = nextCells,
+                width = this.variables.width,
+                height = this.variables.height
+            };
 
-            for (int y = 0; y < height; y++) {
-                int yIndex = y * width;
-                int yUpIndex = (y + 1) * width;
-                int yDownIndex = (y - 1) * width;
-                
-                bool hasUp = (y + 1) < height;
-                bool hasDown = (y - 1) >= 0;
+            JobHandle handle = job.Schedule(length, 64);
+            handle.Complete();
 
-                for (int x = 0; x < width; x++) {
-                    int neighbors = 0;
-                    int xMinus = x - 1;
-                    int xPlus = x + 1;
-                    bool hasLeft = xMinus >= 0;
-                    bool hasRight = xPlus < width;
+            nextCells.CopyTo(grid.Cells);
 
-                    // Row above
-                    if (hasUp) {
-                        if (hasLeft) neighbors += cells[yUpIndex + xMinus];
-                        neighbors += cells[yUpIndex + x];
-                        if (hasRight) neighbors += cells[yUpIndex + xPlus];
-                    }
-
-                    // Current row
-                    if (hasLeft) neighbors += cells[yIndex + xMinus];
-                    if (hasRight) neighbors += cells[yIndex + xPlus];
-
-                    // Row below
-                    if (hasDown) {
-                        if (hasLeft) neighbors += cells[yDownIndex + xMinus];
-                        neighbors += cells[yDownIndex + x];
-                        if (hasRight) neighbors += cells[yDownIndex + xPlus];
-                    }
-                    
-                    this.neighborsGrid[yIndex + x] = neighbors;
-                }
-            }
+            currentCells.Dispose();
+            nextCells.Dispose();
         }
         
-        /// <summary>
-        /// Update the population of the grid
-        /// based on the neighbors <br />
-        /// <list type="bullet">
-        /// <item>
-        /// Any live cell with fewer than two live neighbors dies, as if by underpopulation.<br />
-        /// </item>
-        /// <item>
-        /// Any live cell with two or three live neighbors lives on to the next generation.<br />
-        /// </item>
-        /// <item>
-        /// Any live cell with more than three live neighbors dies, as if by overpopulation.<br />
-        /// </item>
-        /// <item>
-        /// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-        /// </item>
-        /// </list>
-        /// </summary>
-        /// <param name="grid"></param>
-        private void ControlPopulation(IGrid grid) {
-            this.population = 0;
-            int[] cells = grid.Cells;
-            int length = cells.Length;
-            
-            for (int i = 0; i < length; i++) {
-                int cell = cells[i];
-                int neighbors = this.neighborsGrid[i];
+        [BurstCompile]
+        private struct SimulationJob : IJobParallelFor {
+            [ReadOnly] public NativeArray<int> currentCells;
+            public NativeArray<int> nextCells;
+            public int width;
+            public int height;
 
-                if (cell == 1) {
-                    if (neighbors < 2 || neighbors > 3) {
-                        cells[i] = 0;
-                    } else {
-                        this.population++;
+            public void Execute(int index) {
+                int y = index / this.width;
+                int x = index % this.width;
+                int neighbors = 0;
+
+                // For loops only work for -1 and 1
+                // Ignore i == 0 and j == 0 because that is the current cell
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        if (i == 0 && j == 0) continue;
+
+                        int neighborX = x + i;
+                        int neighborY = y + j;
+
+                        if (neighborX >= 0 && neighborX < this.width && neighborY >= 0 && neighborY < this.height) {
+                            neighbors += this.currentCells[neighborY * this.width + neighborX];
+                        }
                     }
-                } else if (neighbors == 3) {
-                    cells[i] = 1;
-                    this.population++;
+                }
+
+                int currentState = this.currentCells[index];
+                if (currentState == 1) {
+                    this.nextCells[index] = (neighbors == 2 || neighbors == 3) ? 1 : 0;
+                } else {
+                    this.nextCells[index] = (neighbors == 3) ? 1 : 0;
                 }
             }
         }
