06aacba5554ba1275a7f31e726a7bcd197bf2e01
Jesus Castro <jjoesei.castro@gmail.com>
2025-12-23 21:30:57 +0800

    Also Inject GridOverlay

--- Assets/Scripts/ScriptableObjects/Variables.cs
+++ Assets/Scripts/ScriptableObjects/Variables.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using Views;
 
-namespace ScriptableObjects {
+namespace ScriptableObjects
+{
     [CreateAssetMenu(fileName = "Variables", menuName = "Variables")]
-    public class Variables : ScriptableObject {
+    public class Variables : ScriptableObject
+    {
+        [Header("Required")]
         public CellView cellPrefab;
+
+        [Header("Simulation Settings")]
         public int width;
         public int height;
         [Range(0.1f, 100f)]
         public float speed;
+
+        [Header("Grid Settings")]
+        public Color mainColor;
+        public Color subColor;
+        public bool showMainGrid;
+        public bool showSubGrid;
+        public float smallStep;
+        public float largeStep;
     }
 }
--- Assets/Scripts/UI/GridOverlay.cs
+++ Assets/Scripts/UI/GridOverlay.cs
@@ -1,93 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using ScriptableObjects;
 using UnityEngine;
 using UnityEngine.Rendering;
+using VContainer;
 
 public class GridOverlay : MonoBehaviour
 {
-    Material lineMaterial;
+    private static readonly string INTERNAL_SHADER_TAG = "Hidden/Internal-Colored";
+    private static readonly int SRC_BLEND = Shader.PropertyToID("_SrcBlend");
+    private static readonly int DST_BLEND = Shader.PropertyToID("_DstBlend");
+    private static readonly int Z_WRITE = Shader.PropertyToID("_ZWrite");
+    private static readonly int CULL = Shader.PropertyToID("_Cull");
+    private Material lineMaterial;
 
-    public Color subColor;
-    public Color mainColor;
+    private Color subColor;
+    private Color mainColor;
 
-    public bool showMain = true;
-    public bool showSub = false;
-    public float smallStep, largeStep;
+    private bool showMain = true;
+    private bool showSub = false;
+    private float smallStep, largeStep;
 
-    [SerializeField]
     private Variables variables;
-    int gridSizeX, gridSizeY;
-    float startX = -0.5f, startY = -0.5f;
+    private int gridSizeX, gridSizeY;
+    private float startX = -0.5f, startY = -0.5f;
 
-    void Start()
-    {
+    [Inject]
+    public void Configure(Variables variables) {
+        this.variables = variables;
         UpdateValues();
     }
-
+    
     void UpdateValues() {
-        gridSizeX = this.variables.width;
-        gridSizeY = this.variables.height;
+        this.gridSizeX = this.variables.width;
+        this.gridSizeY = this.variables.height;
+        this.subColor = this.variables.subColor;
+        this.mainColor = this.variables.mainColor;
+        this.smallStep = this.variables.smallStep;
+        this.largeStep = this.variables.largeStep;
+        this.showMain = this.variables.showMainGrid;
+        this.showSub = this.variables.showSubGrid;
     }
 
     void CreateLineMaterial()
     {
-        if (!lineMaterial)
+        if (!this.lineMaterial)
         {
-            Shader shader = Shader.Find("Hidden/Internal-Colored");
-            lineMaterial = new Material(shader);
+            Shader shader = Shader.Find(INTERNAL_SHADER_TAG);
+            this.lineMaterial = new Material(shader) {
+                hideFlags = HideFlags.HideAndDontSave
+            };
 
-            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+            this.lineMaterial.SetInt(SRC_BLEND, (int)BlendMode.SrcAlpha);
+            this.lineMaterial.SetInt(DST_BLEND, (int)BlendMode.OneMinusSrcAlpha);
 
-            lineMaterial.SetInt("_SrcBlend", (int)BlendMode.SrcAlpha);
-            lineMaterial.SetInt("_DstBlend", (int)BlendMode.OneMinusSrcAlpha);
-
-            lineMaterial.SetInt("_ZWrite", 0);
-            lineMaterial.SetInt("_Cull", (int)CullMode.Off);
+            this.lineMaterial.SetInt(Z_WRITE, 0);
+            this.lineMaterial.SetInt(CULL, (int)CullMode.Off);
         }
     }
 
     private void OnDisable()
     {
-        DestroyImmediate(lineMaterial);
+        DestroyImmediate(this.lineMaterial);
     }
 
     private void OnPostRender()
     {
         CreateLineMaterial();
 
-        lineMaterial.SetPass(0);
+        this.lineMaterial.SetPass(0);
         GL.Begin(GL.LINES);
 
-        if (showSub)
+        if (this.showSub)
         {
-            GL.Color(subColor);
-            for (float y = 0; y <= gridSizeY; y += smallStep)
+            GL.Color(this.subColor);
+            for (float y = 0; y <= this.gridSizeY; y += this.smallStep)
             {
-                GL.Vertex3(startX, startY + y, 0);
-                GL.Vertex3(startX + gridSizeX, startY + y, 0);
+                GL.Vertex3(this.startX, this.startY + y, 0);
+                GL.Vertex3(this.startX + this.gridSizeX, this.startY + y, 0);
             }
 
-            for (float x = 0; x <= gridSizeX; x += smallStep)
+            for (float x = 0; x <= this.gridSizeX; x += this.smallStep)
             {
-                GL.Vertex3(startX + x, startY, 0);
-                GL.Vertex3(startX + x, startY + gridSizeY, 0);
+                GL.Vertex3(this.startX + x, this.startY, 0);
+                GL.Vertex3(this.startX + x, this.startY + this.gridSizeY, 0);
             }
         }
 
-        if (showMain)
+        if (this.showMain)
         {
-            GL.Color(mainColor);
-            for (float y = 0; y <= gridSizeY; y += largeStep)
+            GL.Color(this.mainColor);
+            for (float y = 0; y <= this.gridSizeY; y += this.largeStep)
             {
-                GL.Vertex3(startX, startY + y, 0);
-                GL.Vertex3(startX + gridSizeX, startY + y, 0);
+                GL.Vertex3(this.startX, this.startY + y, 0);
+                GL.Vertex3(this.startX + this.gridSizeX, this.startY + y, 0);
             }
 
-            for (float x = 0; x <= gridSizeX; x += largeStep)
+            for (float x = 0; x <= this.gridSizeX; x += this.largeStep)
             {
-                GL.Vertex3(startX + x, startY, 0);
-                GL.Vertex3(startX + x, startY + gridSizeY, 0);
+                GL.Vertex3(this.startX + x, this.startY, 0);
+                GL.Vertex3(this.startX + x, this.startY + this.gridSizeY, 0);
             }
         }
 
